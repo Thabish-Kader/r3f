13b4d822111418b3022232a29190911a8f090854
Thabish <kadertabish2@gmail.com>
2023-02-18 09:41:59 +0400

    rotation-done
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- room/src/components/Camera.tsx
+++ room/src/components/Camera.tsx
@@ -1,13 +1,31 @@
 import { useGLTF } from "@react-three/drei";
 import { GLTFResult } from "../typings";
-
+import { useControls } from "leva";
 export const Camera = () => {
 	const { nodes, materials } = useGLTF("../../public/room.glb") as GLTFResult;
+	const { position, rotation } = useControls({
+		position: {
+			value: { x: -0.59, y: 83, z: -0.04 },
+			step: 0.01,
+		},
+		rotation: {
+			value: { x: 1.6, y: 0.06, z: 1.06 },
+			step: 0.01,
+		},
+	});
 	return (
-		<mesh
-			geometry={nodes.Camera.geometry}
-			material={nodes.Camera.material}
-			position={[0, 1, 0]}
-		/>
+		<group
+			position={[position.x, position.y, position.z]}
+			rotation={[rotation.x, rotation.y, rotation.z]}
+		>
+			<mesh
+				geometry={nodes.Camera.geometry}
+				material={nodes.Camera.material}
+			/>
+			<mesh
+				geometry={nodes.Camera_1.geometry}
+				material={materials.Lens}
+			/>
+		</group>
 	);
 };
